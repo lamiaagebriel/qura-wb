de513abcb64b8fe3f253e0c03112c56b0587ae84
Lamiaa Gebriel <lamiaadev@gmail.com>
2024-10-28 03:43:27 +0300

    upgrade: aws3

--- src/lib/aws.ts
+++ src/lib/aws.ts
@@ -1,20 +1,16 @@
-import AWS from "aws-sdk";
-import { PutObjectRequest } from "aws-sdk/clients/s3";
+import AWS, { PutObjectCommand, PutObjectCommandInput } from "@aws-sdk/client-s3";
 
-const s3Config = new AWS.S3({
+const s3Config = new AWS.S3Client({
 	region: process.env.AWS_SPACE_REGION,
 	credentials: {
 		accessKeyId: process.env.AWS_SPACE_ACCESS_KEY_ID!,
 		secretAccessKey: process.env.AWS_SPACE_ACCESS_SECRET_KEY!,
 	},
-	httpOptions: { timeout: 60000 },
-	s3ForcePathStyle: true,
-	sslEnabled: true,
 });
 
-async function upload({ Key, ...props }: Omit<PutObjectRequest, "Bucket">) {
+async function upload({ Key, ...props }: Omit<PutObjectCommandInput, "Bucket">) {
 	// Set up S3 upload parameters
-	const params: PutObjectRequest = {
+	const params: PutObjectCommandInput = {
 		Bucket: process.env.AWS_SPACE_BUCKET!,
 		Key: `${Key ?? ""}${Date.now()}`, // Unique key for the file
 		ACL: "public-read", // Make the file publicly accessible,
@@ -22,12 +18,13 @@ async function upload({ Key, ...props }: Omit<PutObjectRequest, "Bucket">) {
 	};
 
 	console.log(params);
-	const result = await s3Config.upload(params).promise();
-	return result?.["Location"];
+	await s3Config.send(new PutObjectCommand(params));
+	return `https://${params?.["Bucket"]}.s3.${process.env.AWS_SPACE_REGION}.amazonaws.com/${params?.["Key"]}`;
 }
 
 const aws = {
 	s3Config,
 	upload,
 };
+
 export { aws };
