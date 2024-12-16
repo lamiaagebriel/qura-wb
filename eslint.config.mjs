2b1f2e3ffd065b49fec964dfb405140067c7343f
Lamiaa Gebriel <lamiaadev@gmail.com>
2024-12-16 22:03:40 +0200

    init: eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  {
+    rules: {
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
