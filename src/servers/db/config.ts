a00f3c1a83eb0411e0af03604cc9a36801c0360b
Lamiaa Gebriel <lamiaadev@gmail.com>
2024-12-17 06:20:10 +0200

    init: drizzle db with schema

---
+++ src/servers/db/config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  dialect: "postgresql",
+  out: "./src/servers/db/migrates",
+  schema: "./src/servers/db/schema.ts",
+  dbCredentials: { url: process.env.DATABASE_URL! },
+});
