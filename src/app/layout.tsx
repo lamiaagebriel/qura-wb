5dac3d413749cb5dc772776bc541c26f60de3b16
Lamiaa Gebriel <lamiaadev@gmail.com>
2026-05-12 00:27:28 +0100

    init shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,11 @@
 import "./globals.css";
 
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import { cn } from "@/lib/utils";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +20,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={cn("h-full", "antialiased", "font-sans", inter.variable)}
     >
       <body className="flex min-h-full flex-col">{children}</body>
     </html>
